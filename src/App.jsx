eccd952b0df30f3425fcf644af02f34d15886a74
MD TAUFIK <110840076+Huntertk@users.noreply.github.com>
2023-10-18 20:20:15 +0530

    This is first commit in this react vite is setup with daisy ui with tailwind css along with pages compononeted initail created and exported as default

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { 
+  About, 
+  Cart, 
+  Checkout, 
+  Error, 
+  HomeLayout, 
+  Landing, 
+  Orders, 
+  Products, 
+  SingleProduct, 
+  Login,
+  Register
+} from './pages/index'
+
+const App = () => {
+  return (
+    <h1 className='text-white bg-slate-950'>App</h1>
+  )
+}
+
+export default App
