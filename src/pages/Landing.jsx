ab8d7d5a275f52eeb619b32a5987d62dcf8dd54a
MD TAUFIK <110840076+Huntertk@users.noreply.github.com>
2023-10-19 17:02:25 +0530

    In this commit initial layout completed along with styling

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Hero } from '../components/index'
+
 
 const Landing = () => {
   return (
-    <h1 className='text-4xl'>Landing</h1>
+    <>
+    <Hero />
+    </>
   )
 }
 
